4b5183255ad7c8d890231a37176acc4e31c2d0f6
sanwazi <chu.jing@uwlax.edu>
2015-04-13 14:56:02 -0500

    admin finished

--- src/main/java/com/example/EMR_Admin/admin/controller/AdminController.java
+++ src/main/java/com/example/EMR_Admin/admin/controller/AdminController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.EMR_Admin.admin.service.AdminService;
 import com.example.EMR_Admin.authentication.data.Admin;
+import com.example.EMR_Admin.authentication.service.CustomUserDetailsService;
 
 @Controller
 public class AdminController {
@@ -80,5 +82,11 @@ public class AdminController {
 			return "s";
 		else
 			return "d";
+	}
+	
+	@RequestMapping(value="/admin/currentAdmin", method = RequestMethod.GET)
+	@Secured(value={"ROLE_ADMIN"})
+	public @ResponseBody UserDetails getCurrentAdmin(){
+		return CustomUserDetailsService.currentUserDetails();
 	}
 }
--- src/main/webapp/WEB-INF/views/js/ourjs/admin.js
+++ src/main/webapp/WEB-INF/views/js/ourjs/admin.js
@@ -1,3 +1,4 @@
+var currentAdmin;
 $(document).ready(function() {
 	loadAdmin();
 	addAdmin();
@@ -17,77 +18,86 @@ function loadAdmin() {
 
 function loadAdminData(admin) {
 	var dataSet = [];
-	for (var i = 0; i < admin.length; i++) {
-		var admins = [];
-		admins.push(admin[i].adminAccount);
-		var deleteButton = "<a name=\"admin_delete"
-				+ "id=\""
-				+ admin[i].adminId
-				+ "\" class=\"btn btn-danger btn-xs\" href=\"delete_admin.html?admin_id="
-				+ admin[i].adminId + "\""
-				+ " ><i class=\"fa fa-trash-o fa-lg\"></i> Delete</a>";
-		admins.push(deleteButton);
-		dataSet.push(admins);
-	}
+	$
+			.ajax({
+				type : "GET",
+				url : "/EMR_Admin/admin/currentAdmin",
+				success : function(data) {
+					currentAdmin = data.username;
+					for (var i = 0; i < admin.length; i++) {
+						if (admin[i].adminAccount != currentAdmin) {
+							var admins = [];
+							admins.push(admin[i].adminAccount);
+							var deleteButton = "<a name=\"admin_delete"
+									+ "id=\""
+									+ admin[i].adminId
+									+ "\" class=\"btn btn-danger btn-xs\" href=\"delete_admin.html?admin_id="
+									+ admin[i].adminId
+									+ "\""
+									+ " ><i class=\"fa fa-trash-o fa-lg\"></i> Delete</a>";
+							admins.push(deleteButton);
+							dataSet.push(admins);
+						}
+					}
 
-	$('#dataTables-admin').DataTable({
-		"responsive" : true,
-		"data" : dataSet,
-		"columns" : [ {
-			"title" : "Admin account",
-			"class" : "center"
-		}, {
-			"title" : "Delete",
-			"class" : "center"
-		} ]
-	});
+					$('#dataTables-admin').DataTable({
+						"responsive" : true,
+						"data" : dataSet,
+						"columns" : [ {
+							"title" : "Admin account",
+							"class" : "center"
+						}, {
+							"title" : "Delete",
+							"class" : "center"
+						} ]
+					});
+				},
+				dataType : "json",
+			});
+
 }
 
 function addAdmin() {
-	$('#addAdmin').on(
-			'click',
-			function() {
-				var account = $('#adminAccount').val();
-				var password1 = $('#password1').val();
-				var password2 = $('#password2').val();
+	$('#addAdmin').on('click', function() {
+		var account = $('#adminAccount').val();
+		var password1 = $('#password1').val();
+		var password2 = $('#password2').val();
 
-				if (account == "") {
-					$('#addingResult').html("Input admin account please!");
-					$('#addingResult').show();
-					return;
-				}
-				if (password1 == "") {
-					$('#addingResult').html("Input password please!");
-					$('#addingResult').show();
-					return;
-				}
+		if (account == "") {
+			$('#addingResult').html("Input admin account please!");
+			$('#addingResult').show();
+			return;
+		}
+		if (password1 == "") {
+			$('#addingResult').html("Input password please!");
+			$('#addingResult').show();
+			return;
+		}
 
-				if (password1 != password2) {
-					$('#addingResult').html("You input different passwords.");
+		if (password1 != password2) {
+			$('#addingResult').html("You input different passwords.");
+			$('#addingResult').show();
+			return;
+		}
+
+		$.ajax({
+			type : "GET",
+			url : "/EMR_Admin/admin/addAdmin",
+			data : 'account=' + account + '&password=' + password1,
+			success : function(data) {
+				// TODO
+				if (data == "s") {
+					$('#addingResult').html("Success!");
 					$('#addingResult').show();
-					return;
+					location.reload(true);
+				} else if (data == "d") {
+					$('#addingResult').html("Failure! Duplicated Name!");
+					$('#addingResult').show();
 				}
-				
-				$.ajax({
-					type : "GET",
-					url : "/EMR_Admin/admin/addAdmin",
-					data : 'account=' + account
-							+ '&password=' + password1,
-					success : function(data) {
-						// TODO
-						if (data == "s") {
-							$('#addingResult').html("Success!");
-							$('#addingResult').show();
-							location.reload(true);
-						} else if (data == "d") {
-							$('#addingResult')
-									.html("Failure! Duplicated Name!");
-							$('#addingResult').show();
-						}
-					},
-					dataType : "text",
-				});
-			});
+			},
+			dataType : "text",
+		});
+	});
 }
 function showAddingAdminTable() {
 	$("#btn-showAddingAdminTable").on('click', function() {
